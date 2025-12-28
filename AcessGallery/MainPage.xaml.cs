d0625b810461875de357f7913a073d6b2a191c3c
Rafael Fernandes <marcorafaeldemelo16@protonmail.com>
2025-12-28 00:48:02 -0300

    solicitando pernição

--- AcessGallery/MainPage.xaml.cs
+++ AcessGallery/MainPage.xaml.cs
@@ -13,12 +13,42 @@ public partial class MainPage : ContentPage
         BindingContext = _viewModel;
 	}
 
-    protected override void OnAppearing()
+    protected override async void OnAppearing()
     {
         base.OnAppearing();
+
+        await CheckAndRequestPermissionsAsync();
+
         if (_viewModel.LoadPhotosCommand.CanExecute(null))
         {
             _viewModel.LoadPhotosCommand.Execute(null);
+        }
+    }
+
+    private async Task CheckAndRequestPermissionsAsync()
+    {
+        try
+        {
+            PermissionStatus status = await Permissions.CheckStatusAsync<Permissions.StorageRead>();
+            
+            if (status != PermissionStatus.Granted)
+            {
+                status = await Permissions.RequestAsync<Permissions.StorageRead>();
+            }
+
+            if (DeviceInfo.Platform == DevicePlatform.Android && DeviceInfo.Version.Major >= 13)
+            {
+                var statusPhotos = await Permissions.CheckStatusAsync<Permissions.Photos>();
+                if (statusPhotos != PermissionStatus.Granted)
+                {
+                    await Permissions.RequestAsync<Permissions.Photos>();
+                }
+                
+            }
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine($"Error requesting permissions: {ex.Message}");
         }
     }
 }
